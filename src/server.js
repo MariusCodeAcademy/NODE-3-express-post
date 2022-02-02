bb793dce993579aae1a214789b67f2516bf86dfe
Marius <marius@example.com>
2022-02-02 13:57:31 +0200

    server setup + start script

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('server.js');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+
+// Routes
+app.get('/test', (request, response) => {
+  // vyks kodas kai nueisim i localhost:PORT/test
+  response.send('<h2> Back end on line </h2>');
+});
+
+// Server
+app.listen(PORT, console.log(`Server is runnig on port ${PORT}`));
