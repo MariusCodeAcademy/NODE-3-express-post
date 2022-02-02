479c7b24ace5ca84831bfe92b891fb3d6f2fdb5f
Marius <marius@example.com>
2022-02-02 14:01:08 +0200

    posts arr

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+const posts = [
+  { id: 1, title: 'First post title', text: 'Text of First Post' },
+  { id: 2, title: 'Second post title', text: 'Text of Second Post' },
+  { id: 3, title: 'Thrid post title', text: 'Text of Thrid Post' },
+];
+
 // Middleware
 
 // Routes
